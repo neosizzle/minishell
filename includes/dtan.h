0c6a000d1895ab9c2f6f3846dc90bb88d1ef3908
neosizzle <junahnngd1115@gmail.com>
2021-08-22 17:00:22 +0800

    quote support added

--- includes/dtan.h
+++ includes/dtan.h
@@ -3,9 +3,5 @@
 
 # include <unistd.h>
 
-typedef struct s_env {
-	char			*content;
-	struct s_env	*next;
-}				t_env;
 
 #endif
---
+++ srcs/parser/ft_split_custom.c
@@ -0,0 +1,89 @@
+#include "minishell.h"
+static void	ft_strcpy(char *dst, char *begin, char *end)
+{
+	while (begin < end)
+		*(dst++) = *(begin++);
+	*dst = 0;
+}
+
+static int	handle_quotes_tw(char **s)
+{
+	char	quote;
+
+	quote = **s;
+	if (!(quote == '\"' || quote == '\''))
+		return (0);
+	(*s)++;
+	while (**s && **s != quote)
+		(*s)++;
+	if (**s == quote)
+		(*s)++;
+	return (1);
+}
+
+static int	get_tw(char *s, char c)
+{
+	int		res;
+
+	res = 0;
+	while (*s)
+	{
+		if (!(*s == c))
+		{
+			res++;
+			if (handle_quotes_tw(&s))
+				continue ;
+			while (*s && !(*s == c))
+				s++;
+		}
+		else
+			s++;
+	}
+	return (res);
+}
+
+static int	handle_quotes(char **res, char *start, int *i, char **s)
+{
+	char	quote;
+
+	quote = **s;
+	if (!(quote == '\"' || quote == '\''))
+		return (0);
+	(*s)++;
+	while (**s && **s != quote)
+		(*s)++;
+	if (**s == quote)
+		(*s)++;
+	res[*i] = (char *)malloc((char *)*s - start + 1);
+	ft_strcpy(res[(*i)++], start, (char *)*s);
+	return (1);
+}
+
+char	**ft_split_custom(char *s, char c)
+{
+	char	*start;
+	char	**res;
+	int		i;
+
+	if (!s)
+		return (0);
+	res = (char **)malloc((sizeof(char *) * (get_tw((char *)s, c) + 1)));
+	i = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			start = (char *)s;
+			if (handle_quotes(res, start, &i, &s))
+				continue ;
+			while (*s && *s != c)
+				s++;
+			res[i] = (char *)malloc((char *)s - start + 1);
+			ft_strcpy(res[i++], start, (char *)s);
+		}
+		else
+			s++;
+	}
+	res[i] = 0;
+	return (res);
+}
